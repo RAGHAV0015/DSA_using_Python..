da133351ea691a26746695bd9a6f82fc26f245d6
RAGHAV0015 <raghavsumanth0015@gmail.com>
2024-09-17 17:31:06 +0530

    is added insertion and delete function

--- OneDrive/Desktop/DSA/linked_list.py
+++ OneDrive/Desktop/DSA/linked_list.py
@@ -3,8 +3,10 @@ class Node:
         self.data =data #n1.next =5
         self.next =next # n1.next =None
 class Sll:
+    
     def __init__(self,head =None):
-        self.head =head #sll.head
+        self.head =head #sll.head =none
+    
     def traverse(self):
         if self.head is None:
             print("linkled list is empty")
@@ -14,8 +16,61 @@ class Sll:
                 print(a.data,end ='-->') #n1.data,n2.data,n3.data,
                 a=a.next #n1.next =n2
             print(None)
+    def insert_at_beginning(self,data):
+    
+        nb =Node(data)
+        nb.next =self.head #will create a link between nb and n1 node
+        self.head =nb # as head will piont to the first node therefore now head will point to wards nb
+    
+    def insert_at_end(self,data):
+        ne =Node(data)
+        a =self.head
+        while a.next is not None:
+            a =a.next
+        a.next =ne
+    
+    def insert_at_specified(self,data,position):
+        nas =Node(data)
+        a =self.head
+        for i in range(1,position-1):
+            a =a.next
+        nas.next =a.next
+        a.next =nas
+    
+    def del_at_beginning(self):
+        a =self.head
+        self.head=a.next   
+        a.next =None
+    
+    def del_at_ending(self):
+        a=self.head.next
+        pre =self.head
+        while a.next is not None:
+            a =a.next
+            pre =pre.next
+        pre.next =None
+    def del_at_specific(self,position):
+        
+        a =self.head.next
+        pre =self.head
+        for i in range(1,position-1):
+            a =a.next
+            pre =pre.next
+        pre.next =a.next
+        a.next =None
+
+
+        
+        
+
+
+
+
+
+
+
 n1 =Node(5)
-n2 =Node(9)
+n2 =Node(9) 
 n3 =Node(7)
 n4 =Node(1)
 sll =Sll() #sll.head = none
@@ -24,5 +79,13 @@ n1.next =n2
 n2.next =n3
 n3.next =n4
 sll.traverse()
-
-
+sll.insert_at_beginning(6)
+sll.traverse()
+sll.insert_at_end(10)
+sll.traverse()
+sll.insert_at_specified(8,2)
+sll.traverse()
+sll.del_at_beginning()
+sll.traverse()
+sll.del_at_specific(4)
+sll.traverse() 
